f2eb7b5c4e333204d65b52b1b6046c1369beedd2
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-04-12 23:22:38 +0300

    Solved Task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+  id: 'vimeo-player',
+});
+
+const TIME_DATA_NAME = 'videoplayer-current-time';
+
+if (localStorage.getItem(TIME_DATA_NAME)) {
+  const currentTime = JSON.parse(localStorage.getItem(TIME_DATA_NAME));
+  player.setCurrentTime(currentTime.seconds);
+}
+
+const onPlay = data => {
+  localStorage.setItem(TIME_DATA_NAME, JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
